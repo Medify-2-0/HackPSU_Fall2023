55f2e0b4eedb58f152a4aa4132c1fc1a6f1d4938
harsh-185 <harsh.maheshwari180504@gmail.com>
2023-10-21 16:43:57 -0400

    dataset updated

--- extract.py
+++ extract.py
@@ -1,44 +1,27 @@
 import os
 import shutil
-import random
 
 # Paths for the dataset
-base_path = ""
-data = []
-
+base_path_no = "brain_tumor_dataset/no"
+base_path_yes = "brain_tumor_dataset/yes"
+images_no=[file for file in os.listdir(base_path_no)]
+images_yes=[file for file in os.listdir(base_path_yes)]
 
 train_ratio = 0.7
 valid_ratio = 0.15
 test_ratio = 0.15
 
-if not os.path.exists(os.path.join(base_path, 'train')):
-    os.makedirs(os.path.join(base_path, 'train', 'tumor'))
-    os.makedirs(os.path.join(base_path, 'train', 'cancer'))
+num_yes=len(images_yes)
+num_no=len(images_no)
 
-if not os.path.exists(os.path.join(base_path, 'valid')):
-    os.makedirs(os.path.join(base_path, 'valid', 'tumor'))
-    os.makedirs(os.path.join(base_path, 'valid', 'cancer'))
+def move(files, source, destination):
+    for file in files:
+        shutil.move(os.path.join(source, file), os.path.join(destination, file))
 
-if not os.path.exists(os.path.join(base_path, 'test')):
-    os.makedirs(os.path.join(base_path, 'test', 'tumor'))
-    os.makedirs(os.path.join(base_path, 'test', 'cancer'))
+move(images_no[:int(train_ratio*(num_no))],base_path_no, 'training_tumor')
+move(images_no[int(train_ratio*(num_no)):int((train_ratio+valid_ratio)*(num_no))],base_path_no, 'validating_tumor')
+move(images_no[int((train_ratio+valid_ratio)*(num_no)):],base_path_no, 'testing_tumor')
 
-
-random.shuffle(data)
-
-
-train_data = data[:int(len(data) * train_ratio)]
-valid_data = data[int(len(data) * train_ratio):int(len(data) * (train_ratio + valid_ratio))]
-test_data = data[int(len(data) * (train_ratio + valid_ratio)):]
-
-
-def move(data_list, subset):
-    for dcm_path, jpg_path, label in data_list:
-        dest_path = os.path.join(base_path, subset, label, os.path.basename(jpg_path))
-        shutil.copy(jpg_path, dest_path)
-
-move(train_data, 'train')
-move(valid_data, 'valid')
-move(test_data, 'test')
-
-print("Dataset organized successfully!")
+move(images_yes[:int(train_ratio*(num_yes))],base_path_yes, 'training_tumor')
+move(images_yes[int(train_ratio*(num_yes)):int((train_ratio+valid_ratio)*(num_yes))],base_path_yes, 'validating_tumor')
+move(images_yes[int((train_ratio+valid_ratio)*(num_yes)):],base_path_yes, 'testing_tumor')
