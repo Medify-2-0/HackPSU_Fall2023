4da41ff78c20c3cc6b6e5ae451276bc50cc10e94
harsh-185 <harsh.maheshwari180504@gmail.com>
2023-10-22 04:04:09 -0400

    readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # HackPSU2023
+
+Datasets:
+1) https://www.kaggle.com/datasets/yasserhessein/dataset-alzheimer/data
+2) https://www.kaggle.com/datasets/navoneel/brain-mri-images-for-brain-tumor-detection
--- alzeihmers/model.py
+++ alzeihmers/model.py
@@ -1,51 +1,51 @@
+from re import split
 from numpy import resize
+from sympy import Max
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers
-from keras.applications import VGG16
+from keras.applications import ResNet50
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Dropout, Conv2D, MaxPooling2D, Activation
+from keras.layers import Dense, Flatten, Dropout, Conv2D, MaxPooling2D, Activation, GlobalAveragePooling2D
 
-base_model=VGG16(include_top=False)
-
-data_gen=ImageDataGenerator(rescale=1./255, horizontal_flip=True)
+data_gen=ImageDataGenerator(rescale=1./255, validation_split=0.2, horizontal_flip=True, shear_range=0.5, brightness_range=[0.5,1.0])
 
 train= data_gen.flow_from_directory(
     directory= "alzeihmers/train",
     target_size=(224,224),
+    batch_size=32,
     shuffle=True,
-    batch_size=64
+    subset='training',
+
 )
 
 valid=data_gen.flow_from_directory(
     directory= "alzeihmers/train",
     target_size=(224,224),
-    batch_size=64
+    batch_size=32,
+    subset='validation'
 )
 
 test=data_gen.flow_from_directory(
     directory= "alzeihmers/test",
     target_size=(224,224),
-    batch_size=64
+    batch_size=32
 )
 
 
 model = Sequential()
-model.add(base_model)
+model.add(Conv2D(16, (3,3), activation="relu", input_shape=(224,224,3)))
+model.add(MaxPooling2D(2,2))
+model.add(Conv2D(32, (3,3), activation="relu"))
+model.add(MaxPooling2D(2,2))
+model.add(Dropout(0.2))
+model.add(Conv2D(64, (3,3), activation="relu"))
+model.add(MaxPooling2D(2,2))
+model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Conv2D(32, (3,3), input_shape = (224,224,3), activation="relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Conv2D(32, (3,3), input_shape = (224,224,3), activation="relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Conv2D(64, (3,3), input_shape = (224,224,3), activation="relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Conv2D(128, (3,3), input_shape = (224,224,3), activation="relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Flatten())
-model.add(Dense(256))
-model.add(Activation("relu"))
-model.add(Dropout(0.5))
+model.add(Dense(128, activation="relu", kernel_initializer="he_normal"))
+model.add(Dense(64, activation="relu"))
 model.add(Dense(4, activation="softmax"))
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
               loss='categorical_crossentropy',
@@ -57,12 +57,16 @@ early_stopping = keras.callbacks.EarlyStopping(
     min_delta=0.001,
     restore_best_weights=True)
 
+class_weights={0: 3, 1: 20, 2: 1, 3: 1.5}
+
 history = model.fit(
     train,
     validation_data= valid,
     validation_split=0.2,
-    batch_size = 128,
-    epochs=20
+    batch_size = 64,
+    epochs=20,
+    verbose=1,
+    class_weight=class_weights
 )
 
 loss, accuracy = model.evaluate(test)
@@ -72,4 +76,3 @@ print(f"Test Accuracy: {accuracy:.4f}")
 
 model.save('alzeihmers.h5')
 
-#test accuracy 0.9399
